80a0a5f994814f17397eacb5abae3bec2fbcb327
chinared <gooday.alan@foxmail.com>
2024-06-20 17:42:41 +0800

    提交

---
+++ components/content/MyImgList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="w-full flex gap-6 snap-x snap-mandatory overflow-x-auto pb-14"
+  >
+    <div class="snap-center shrink-0">
+      <img
+        src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
+      />
+    </div>
+    <div class="snap-center shrink-0">
+      <img
+        src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
+      />
+    </div>
+    <div class="snap-center shrink-0">
+      <img
+        src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
+      />
+    </div>
+    <div class="snap-center shrink-0">
+      <img
+        src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
+      />
+    </div>
+    <div class="snap-center shrink-0">
+      <img
+        src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
+      />
+    </div>
+    <div class="snap-center shrink-0">
+      <img
+        src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
+      />
+    </div>
+    <div class="snap-center shrink-0">
+      <img
+        src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
+      />
+    </div>
+  </div>
+</template>
--- plugins/checkLog.ts
+++ plugins/checkLog.ts
@@ -1,7 +1,7 @@
 export default defineNuxtPlugin((nuxtApp) => {
   // Vue 3 中使用 Vue.config.warnHandler
   nuxtApp.vueApp.config.warnHandler = (msg, vm, trace) => {
-    if (msg.includes("Hydration class mismatch")) {
+    if (msg.includes("Hydration class mismatch") || msg.includes('Hydration attribute mismatch')) {
       // 忽略特定的警告
       console.log("Ignoring hydration class mismatch warning")
       return
