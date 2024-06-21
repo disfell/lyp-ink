fc4e6ddc8401bc8de39cebf219e1974eba191860
梁某 <gooday.alan@foxmail.com>
2024-06-22 01:06:32 +0800

    bug fix

--- components/content/MyImgList.vue
+++ components/content/MyImgList.vue
@@ -2,7 +2,7 @@
   <div class="p-8 w-full flex gap-6 snap-x snap-mandatory overflow-x-auto bg-stone-50 dark:bg-stone-700 rounded-md border dark:border-stone-400">
     <div v-for="(i, idx) in items" :key="idx" class="snap-center shrink-0 shadow-lg cursor-pointer">
       <a :src="checkImg(i, true)" data-fancybox="gallery" data-caption="lyp.ink">
-        <MyRootImg :src="i" class="content.MyImgList"/>
+        <MyRootImg :src="checkImg(i, true)" class="content.MyImgList"/>
       </a>
     </div>
   </div>
