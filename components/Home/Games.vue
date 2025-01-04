ab07d46093cadfcbd728efca78abee3a01444ef5
disfell <gooday.alan@foxmail.com>
2025-01-04 18:34:00 +0800

    fix game text

--- components/Home/Games.vue
+++ components/Home/Games.vue
@@ -9,7 +9,7 @@
           <a class="flex items-center gap-3 hover:bg-gray-200 dark:hover:bg-white/10 p-2 rounded-lg -m-2 text-sm min-w-0" target="_blank">
             <UAvatar :alt="game.name" :src="getThumbnail(game.game_id)" :ui="{ rounded: 'rounded-md' }" />
             <p class="truncate text-gray-700">
-              {{ `${game.name_cn} - ${game.name}` }}
+              {{ gameTitle(game.name_cn, game.name) }}
             </p>
             <span class="flex-1"></span>
             <span class="text-xs text-gray-400 dark:text-gray-600 min-w-24"> {{ toMinutes(game.play_time) }} </span>
@@ -56,5 +56,13 @@ function toMinutes(src) {
   const hours = Math.floor(durationObj.asHours()); // 获取整数小时数
   const minutes = durationObj.minutes(); // 获取剩余的分钟数
   return `${hours} 小时 ${minutes} 分钟`;
+}
+
+function gameTitle(nameCn, name) {
+  if (nameCn === null) {
+    return name;
+  } else {
+    return nameCn + " - " + name;
+  }
 }
 </script>
