de8b5f3a81d8f80ce67325de592a629f19f0fb98
梁衍鹏 <wxliangyanpeng@gfpartner.com.cn>
2025-03-05 15:48:42 +0800

    增加test页面，更新nuxt版本

--- config/site.js
+++ config/site.js
@@ -5,16 +5,16 @@ export const site = {
   intro: "Hi，我是 LYP，该网站主要记录自己的学习、游戏、心得，捣鼓一些业余的东西。🌵",
   email: "gooday.alan@foxmail.com",
   menus: [
-    { name: "首页", path: "/", icon: "solar:home-smile-outline" },
-    // {
-    //   name: "记录",
-    //   path: "/records",
-    //   icon: "solar:document-add-outline",
-    // },
+    { name: "首页", path: "/", icon: "solar:home-smile-outline" },  
     {
       name: "书签",
       path: "/bookmarks",
       icon: "solar:bookmark-linear",
+    },
+    {
+      name: "测试",
+      path: "/test",
+      icon: "solar:programming-outline",
     },
   ],
 };
---
+++ pages/test.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="min-h-screen">
+    搞点试验....
+  </main>
+</template>
+<script setup>
+useSeoMeta({
+  title: "测试 | " + useAppConfig().site.title,
+  description: useAppConfig().site.intro,
+});
+</script>
